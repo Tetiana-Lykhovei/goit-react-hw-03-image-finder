ef91f9cb4d48c1b185ea9df5c2e6393488daa09b
Tetiana-Lykhovei <tlykhovei@gmail.com>
2021-03-14 03:22:18 +0200

    переписала фетч

--- src/services/PixabayApi.js
+++ src/services/PixabayApi.js
@@ -1,14 +1,21 @@
 import axios from "axios";
 
-async function getFetch(query, page) {
+const getFetch = (query, page) => {
   let key = `19872404-8517b83978ef96fd2196e3bf4`;
-  axios.defaults.baseURL = `https://pixabay.com/api/`;
-  axios.defaults.headers.common["Authorization"] = key;
-  let url = `?q=${query}&page=${page}&key=${key}&image_type=photo&orientation=horizontal&&per_page=12`;
-  const response = await axios.get(url);
-  const data = await response.data;
-  const photos = await data.photos;
-  return photos;
-}
+  return axios.get(
+    `https://pixabay.com/api/?q=${query}&page=${page}&key=${key}&image_type=photo&orientation=horizontal&&per_page=12`
+  );
+};
+
+// async function getFetch(query, page) {
+//   let key = `19872404-8517b83978ef96fd2196e3bf4`;
+//   axios.defaults.baseURL = `https://pixabay.com/api/`;
+//   axios.defaults.headers.common["Authorization"] = key;
+//   let url = `?q=${query}&page=${page}&key=${key}&image_type=photo&orientation=horizontal&&per_page=12`;
+//   const response = await axios.get(url);
+//   const data = await response.data;
+//   const photos = await data.photos;
+//   return photos;
+// }
 
 export default getFetch;
